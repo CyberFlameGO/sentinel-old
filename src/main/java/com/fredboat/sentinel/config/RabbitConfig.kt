c15b24374f33b5872a5a821ee5043e2de552782c
Frederik Mikkelsen <frogkr@gmail.com>
2018-04-06 21:14:07 +0200

    Fix not being able to deserialize

--- src/main/java/com/fredboat/sentinel/config/RabbitConfig.kt
+++ src/main/java/com/fredboat/sentinel/config/RabbitConfig.kt
@@ -1,17 +1,22 @@
 package com.fredboat.sentinel.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.fredboat.sentinel.QueueNames
 import org.springframework.amqp.core.Queue
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
+import org.springframework.amqp.support.converter.MessageConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 open class RabbitConfig {
 
-    /*@Bean
+    @Bean
     open fun jsonMessageConverter(): MessageConverter {
-        return Jackson2JsonMessageConverter()
-    }*/
+        // We must register this Kotlin module to get deserialization to work with data classes
+        return Jackson2JsonMessageConverter(ObjectMapper().registerKotlinModule())
+    }
 
     @Bean
     open fun queue() = Queue(QueueNames.JDA_EVENTS_QUEUE, false)//TODO
