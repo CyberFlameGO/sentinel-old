978309a1cf17a827b08a117b59daf343037b6405
Frederik Mikkelsen <frogkr@gmail.com>
2018-04-06 21:14:35 +0200

    Add a bunch of events for now

--- src/main/java/com/fredboat/sentinel/listeners/JdaRabbitEventListener.kt
+++ src/main/java/com/fredboat/sentinel/listeners/JdaRabbitEventListener.kt
@@ -1,20 +1,23 @@
 package com.fredboat.sentinel.listeners
 
 import com.fredboat.sentinel.QueueNames
-import com.fredboat.sentinel.entities.ShardDisconnectedEvent
-import com.fredboat.sentinel.entities.ShardReadyEvent
-import com.fredboat.sentinel.entities.ShardReconnectedEvent
-import com.fredboat.sentinel.entities.ShardResumedEvent
+import com.fredboat.sentinel.entities.*
+import com.fredboat.sentinel.extension.toEntity
 import com.google.gson.Gson
 import net.dv8tion.jda.core.events.DisconnectEvent
 import net.dv8tion.jda.core.events.ReadyEvent
 import net.dv8tion.jda.core.events.ReconnectedEvent
 import net.dv8tion.jda.core.events.ResumedEvent
+import net.dv8tion.jda.core.events.guild.GuildJoinEvent
+import net.dv8tion.jda.core.events.guild.GuildLeaveEvent
+import net.dv8tion.jda.core.events.guild.voice.GuildVoiceJoinEvent
+import net.dv8tion.jda.core.events.guild.voice.GuildVoiceLeaveEvent
+import net.dv8tion.jda.core.events.guild.voice.GuildVoiceMoveEvent
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent
+import net.dv8tion.jda.core.events.message.priv.PrivateMessageReceivedEvent
 import net.dv8tion.jda.core.hooks.ListenerAdapter
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.amqp.core.Message
-import org.springframework.amqp.core.MessageProperties
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.stereotype.Component
 import java.nio.charset.Charset
@@ -49,13 +52,62 @@ class JdaRabbitEventListener(
         dispatch(ShardReconnectedEvent(event.jda.shardInfo.shardId, event.jda.shardInfo.shardTotal))
     }
 
+    /* Guild events */
+    override fun onGuildJoin(event: GuildJoinEvent) {
+        dispatch(com.fredboat.sentinel.entities.GuildJoinEvent(event.guild.id))
+    }
+
+    override fun onGuildLeave(event: GuildLeaveEvent) {
+        dispatch(com.fredboat.sentinel.entities.GuildLeaveEvent(event.guild.id))
+    }
+
+    /* Voice events */
+    override fun onGuildVoiceJoin(event: GuildVoiceJoinEvent) {
+        dispatch(VoiceJoinEvent(
+                event.guild.id,
+                event.channelJoined.toEntity(),
+                event.member.toEntity()
+        ))
+    }
+
+    override fun onGuildVoiceLeave(event: GuildVoiceLeaveEvent) {
+        dispatch(VoiceLeaveEvent(
+                event.guild.id,
+                event.channelLeft.toEntity(),
+                event.member.toEntity()
+        ))
+    }
+
+    override fun onGuildVoiceMove(event: GuildVoiceMoveEvent) {
+        dispatch(VoiceMoveEvent(
+                event.guild.id,
+                event.channelLeft.toEntity(),
+                event.channelJoined.toEntity(),
+                event.member.toEntity()
+        ))
+    }
+
+    /* Message events */
+    override fun onGuildMessageReceived(event: GuildMessageReceivedEvent) {
+        dispatch(MessageReceivedEvent(
+                event.message.id,
+                event.message.guild.id,
+                event.channel.toEntity(),
+                event.message.contentRaw,
+                event.member.toEntity()
+        ))
+    }
+
+    override fun onPrivateMessageReceived(event: PrivateMessageReceivedEvent) {
+        dispatch(PrivateMessageReceivedEvent(
+                event.message.contentRaw,
+                event.author.toEntity()
+        ))
+    }
+
     private fun dispatch(event: Any) {
-        val props = MessageProperties()
-        props.type = event.javaClass.simpleName
-        props.contentType = "text/plain"
-        val msg = Message("test"/*gson.toJson(event)*/.toByteArray(charset), props)
-        rabbitTemplate.send(QueueNames.JDA_EVENTS_QUEUE, msg)
-        log.info("Sent $msg")
+        rabbitTemplate.convertAndSend(QueueNames.JDA_EVENTS_QUEUE, event)
+        log.info("Sent $event")
     }
 
 
--- src/main/java/com/fredboat/sentinel/listeners/RabbitConsumer.kt
+++ src/main/java/com/fredboat/sentinel/listeners/RabbitConsumer.kt
@@ -1,6 +1,7 @@
 package com.fredboat.sentinel.listeners
 
 import com.fredboat.sentinel.QueueNames
+import com.fredboat.sentinel.entities.ShardDisconnectedEvent
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.amqp.rabbit.annotation.RabbitHandler
@@ -15,8 +16,18 @@ class RabbitConsumer {
         private val log: Logger = LoggerFactory.getLogger(RabbitConsumer::class.java)
     }
 
-    @RabbitHandler
-    fun receive(msg: String) {
+    /*@RabbitHandler
+    fun receive(msg: ShardReadyEvent) {
         log.info("Received '$msg'")
+    }*/
+
+    @RabbitHandler
+    fun receive2(msg: ShardDisconnectedEvent) {
+        log.info("Received (disconnected) '$msg'")
+    }
+
+    @RabbitHandler(isDefault = true)
+    fun default(msg: Any) {
+        log.warn("Unhandled event $msg")
     }
 }
