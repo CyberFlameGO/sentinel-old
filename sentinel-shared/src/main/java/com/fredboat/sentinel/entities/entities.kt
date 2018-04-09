fb13b04025362eff9b8f7efefe67042bc040ca0d
Frederik Mikkelsen <frogkr@gmail.com>
2018-04-09 18:00:03 +0200

    More entities

--- sentinel-shared/src/main/java/com/fredboat/sentinel/entities/entities.kt
+++ sentinel-shared/src/main/java/com/fredboat/sentinel/entities/entities.kt
@@ -1,5 +1,20 @@
 package com.fredboat.sentinel.entities
 
+data class Shard(
+        val id: Int,
+        val total: Int,
+        val status: String
+)
+
+data class Guild(
+        val id: String,
+        val name: String,
+        val owner: Member,
+        val members: List<Member>,
+        val textChannels: List<TextChannel>,
+        val voiceChannels: List<VoiceChannel>
+)
+
 data class User(
         val id: String,
         val name: String,
@@ -12,6 +27,7 @@ data class Member(
         val name: String,
         val discrim: Short,
         val bot: Boolean,
+        val roles: List<Role>,
         val voiceChannel: VoiceChannel?
 )
 
@@ -25,4 +41,10 @@ data class VoiceChannel(
         val id: String,
         val name: String,
         val ourEffectivePermissions: Long
+)
+
+data class Role(
+        val id: String,
+        val name: String,
+        val permissions: Long
 )
--- src/main/java/com/fredboat/sentinel/extension/entityExtensions.kt
+++ src/main/java/com/fredboat/sentinel/extension/entityExtensions.kt
@@ -1,9 +1,7 @@
 package com.fredboat.sentinel.extension
 
-import com.fredboat.sentinel.entities.Member
-import com.fredboat.sentinel.entities.TextChannel
-import com.fredboat.sentinel.entities.User
-import com.fredboat.sentinel.entities.VoiceChannel
+import com.fredboat.sentinel.entities.*
+import net.dv8tion.jda.core.JDA
 import net.dv8tion.jda.core.utils.PermissionUtil
 
 fun net.dv8tion.jda.core.entities.User.toEntity() = User(
@@ -12,12 +10,18 @@ fun net.dv8tion.jda.core.entities.User.toEntity() = User(
         discriminator.toShort(),
         isBot)
 
-fun net.dv8tion.jda.core.entities.Member.toEntity() = Member(
-        user.id,
-        effectiveName,
-        user.discriminator.toShort(),
-        user.isBot,
-        voiceState?.channel?.toEntity())
+fun net.dv8tion.jda.core.entities.Member.toEntity(): Member {
+    val rolesMutable = mutableListOf<Role>()
+    roles.forEach { rolesMutable.add(it.toEntity()) }
+
+    return Member(
+            user.id,
+            effectiveName,
+            user.discriminator.toShort(),
+            user.isBot,
+            rolesMutable,
+            voiceState?.channel?.toEntity())
+}
 
 fun net.dv8tion.jda.core.entities.VoiceChannel.toEntity() = VoiceChannel(
         id,
@@ -28,3 +32,11 @@ fun net.dv8tion.jda.core.entities.TextChannel.toEntity() = TextChannel(
         id,
         name,
         PermissionUtil.getExplicitPermission(this, guild.selfMember))
+
+fun net.dv8tion.jda.core.entities.Role.toEntity() = Role(
+        id,
+        name,
+        permissionsRaw
+)
+
+fun JDA.Status.toEntity() = ShardStatus.valueOf(this.toString())
