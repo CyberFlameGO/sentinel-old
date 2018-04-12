1be4d3b21f59a3274108ae3a576513ebeaa15b6b
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2018-04-12 08:38:43 +0200

    Rename jda-events to sentinel-events

--- sentinel-shared/src/main/java/com/fredboat/sentinel/QueueNames.kt
+++ sentinel-shared/src/main/java/com/fredboat/sentinel/QueueNames.kt
@@ -1,6 +1,6 @@
 package com.fredboat.sentinel
 
 object QueueNames {
-    const val JDA_EVENTS_QUEUE = "jda-events"
+    const val JDA_EVENTS_QUEUE = "sentinel-events"
     const val SENTINEL_REQUESTS_QUEUE = "sentinel-requests"
 }
