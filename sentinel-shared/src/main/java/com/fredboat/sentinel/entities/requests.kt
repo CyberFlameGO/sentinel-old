b8856158e6fa1a8cb005cdcaeb8aab767c61b163
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2018-05-07 23:33:42 +0200

    Validation for permission checks

--- sentinel-shared/src/main/java/com/fredboat/sentinel/entities/requests.kt
+++ sentinel-shared/src/main/java/com/fredboat/sentinel/entities/requests.kt
@@ -38,18 +38,26 @@ class ApplicationInfoRequest
 /** Returns [PermissionCheckResponse]*/
 data class GuildPermissionRequest(
         val guild: Long,
-        val role: Long? = null,  // If present, the role to check (not mutually exclusive)
-        val member: Long? = null,// If present, the member to check (not mutually exclusive)
+        val role: Long? = null,  // If present, the role to check (mutually exclusive)
+        val member: Long? = null,// If present, the member to check (mutually exclusive)
         val rawPermissions: Long
-)
+){
+    init {
+        if (role != null && member != null) throw RuntimeException("Role and member are mutually exclusive")
+    }
+}
 
 /** Returns [PermissionCheckResponse]*/
 data class ChannelPermissionRequest(
         val channel: Long, // The channel to check
-        val role: Long? = null,  // If present, the role to check (not mutually exclusive)
-        val member: Long? = null,// If present, the member to check (not mutually exclusive)
+        val role: Long? = null,  // If present, the role to check (mutually exclusive)
+        val member: Long? = null,// If present, the member to check (mutually exclusive)
         val rawPermissions: Long
-)
+){
+    init {
+        if (role != null && member != null) throw RuntimeException("Role and member are mutually exclusive")
+    }
+}
 
 data class PermissionCheckResponse(
         val missingPermissions: Long,
