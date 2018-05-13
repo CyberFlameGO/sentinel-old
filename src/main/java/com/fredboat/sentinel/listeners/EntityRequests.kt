babef461d64ae9f2b87b28c2aa091a0e5711cc84
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2018-05-13 14:38:31 +0200

    Refactor RPC service classes

--- src/main/java/com/fredboat/sentinel/listeners/EntityRequests.kt
+++ src/main/java/com/fredboat/sentinel/listeners/EntityRequests.kt
@@ -3,16 +3,13 @@ package com.fredboat.sentinel.listeners
 import com.fredboat.sentinel.QueueNames
 import com.fredboat.sentinel.entities.*
 import com.fredboat.sentinel.extension.toEntity
-import com.fredboat.sentinel.extension.toJda
 import net.dv8tion.jda.bot.sharding.ShardManager
 import net.dv8tion.jda.core.JDA
-import net.dv8tion.jda.core.entities.TextChannel
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.amqp.rabbit.annotation.RabbitHandler
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Service
-import java.util.*
 
 @Service
 @RabbitListener(queues = [QueueNames.SENTINEL_REQUESTS_QUEUE])
@@ -45,84 +42,6 @@ class EntityRequests(private val shardManager: ShardManager) {
         if (guild == null) log.error("Received GuildRequest but guild ${request.id} was not found")
 
         return guild
-    }
-
-    @RabbitHandler
-    fun sendMessage(request: SendMessageRequest): SendMessageResponse? {
-        val channel: TextChannel? = shardManager.getTextChannelById(request.channel)
-
-        if (channel == null) {
-            log.error("Received SendMessageRequest for channel ${request.channel} which was not found")
-            return null
-        }
-
-        val msg = when (request.message) {
-            is Message -> channel.sendMessage((request.message as Message).toJda()).complete()
-            is Embed -> channel.sendMessage((request.message as Embed).toJda()).complete()
-            else -> throw IllegalArgumentException("Unknown message type $request")
-        }
-
-        return SendMessageResponse(msg.idLong)
-    }
-
-    @RabbitHandler
-    fun sendPrivateMessage(request: SendPrivateMessageRequest): SendMessageResponse? {
-        val user = shardManager.getUserById(request.recipient)
-        val channel = user.openPrivateChannel().complete(true)
-
-        if (user == null) {
-            log.error("User ${request.recipient} was not found when sending private message")
-            return null
-        }
-
-        val msg = when (request.message) {
-            is Message -> channel.sendMessage((request.message as Message).toJda()).complete()
-            is Embed -> channel.sendMessage((request.message as Embed).toJda()).complete()
-            else -> throw IllegalArgumentException("Unknown message type $request")
-        }
-        return SendMessageResponse(msg.idLong)
-    }
-
-    @RabbitHandler
-    fun editMessage(request: EditMessageRequest) {
-        val channel: TextChannel? = shardManager.getTextChannelById(request.channel)
-
-        if (channel == null) {
-            log.error("Received EditMessageRequest for channel ${request.channel} which was not found")
-            return
-        }
-
-        when (request.message) {
-            is Message -> channel.editMessageById(request.messageId, (request.message as Message).toJda()).queue()
-            is Embed -> channel.editMessageById(request.messageId, (request.message as Embed).toJda()).queue()
-            else -> throw IllegalArgumentException("Unknown message type $request")
-        }
-    }
-
-    @RabbitHandler
-    fun deleteMessages(request: MessageDeleteRequest) {
-        val channel: TextChannel? = shardManager.getTextChannelById(request.channel)
-
-        if (channel == null) {
-            log.error("Received MessageDeleteRequest for channel ${request.channel} which was not found")
-            return
-        }
-
-        val list = LinkedList<String>()
-        request.messages.forEach {list.add(it.toString())}
-        channel.deleteMessagesByIds(list).queue()
-    }
-
-    @RabbitHandler
-    fun sendTyping(request: SendTypingRequest) {
-        val channel: TextChannel? = shardManager.getTextChannelById(request.channel)
-
-        if (channel == null) {
-            log.error("Received SendTypingRequest for channel ${request.channel} which was not found")
-            return
-        }
-
-        channel.sendTyping().queue()
     }
 
     @Suppress("UNUSED_PARAMETER")
---
+++ src/main/java/com/fredboat/sentinel/listeners/MessageRequests.kt
@@ -0,0 +1,101 @@
+package com.fredboat.sentinel.listeners
+
+import com.fredboat.sentinel.QueueNames
+import com.fredboat.sentinel.entities.*
+import com.fredboat.sentinel.extension.toJda
+import net.dv8tion.jda.bot.sharding.ShardManager
+import net.dv8tion.jda.core.entities.TextChannel
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.amqp.rabbit.annotation.RabbitHandler
+import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+@RabbitListener(queues = [QueueNames.SENTINEL_REQUESTS_QUEUE])
+class MessageRequests(private val shardManager: ShardManager) {
+
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(EntityRequests::class.java)
+    }
+
+    @RabbitHandler
+    fun sendMessage(request: SendMessageRequest): SendMessageResponse? {
+        val channel: TextChannel? = shardManager.getTextChannelById(request.channel)
+
+        if (channel == null) {
+            log.error("Received SendMessageRequest for channel ${request.channel} which was not found")
+            return null
+        }
+
+        val msg = when (request.message) {
+            is Message -> channel.sendMessage((request.message as Message).toJda()).complete()
+            is Embed -> channel.sendMessage((request.message as Embed).toJda()).complete()
+            else -> throw IllegalArgumentException("Unknown message type $request")
+        }
+
+        return SendMessageResponse(msg.idLong)
+    }
+
+    @RabbitHandler
+    fun sendPrivateMessage(request: SendPrivateMessageRequest): SendMessageResponse? {
+        val user = shardManager.getUserById(request.recipient)
+        val channel = user.openPrivateChannel().complete(true)
+
+        if (user == null) {
+            log.error("User ${request.recipient} was not found when sending private message")
+            return null
+        }
+
+        val msg = when (request.message) {
+            is Message -> channel.sendMessage((request.message as Message).toJda()).complete()
+            is Embed -> channel.sendMessage((request.message as Embed).toJda()).complete()
+            else -> throw IllegalArgumentException("Unknown message type $request")
+        }
+        return SendMessageResponse(msg.idLong)
+    }
+
+    @RabbitHandler
+    fun editMessage(request: EditMessageRequest) {
+        val channel: TextChannel? = shardManager.getTextChannelById(request.channel)
+
+        if (channel == null) {
+            log.error("Received EditMessageRequest for channel ${request.channel} which was not found")
+            return
+        }
+
+        when (request.message) {
+            is Message -> channel.editMessageById(request.messageId, (request.message as Message).toJda()).queue()
+            is Embed -> channel.editMessageById(request.messageId, (request.message as Embed).toJda()).queue()
+            else -> throw IllegalArgumentException("Unknown message type $request")
+        }
+    }
+
+    @RabbitHandler
+    fun deleteMessages(request: MessageDeleteRequest) {
+        val channel: TextChannel? = shardManager.getTextChannelById(request.channel)
+
+        if (channel == null) {
+            log.error("Received MessageDeleteRequest for channel ${request.channel} which was not found")
+            return
+        }
+
+        val list = LinkedList<String>()
+        request.messages.forEach {list.add(it.toString())}
+        channel.deleteMessagesByIds(list).queue()
+    }
+
+    @RabbitHandler
+    fun sendTyping(request: SendTypingRequest) {
+        val channel: TextChannel? = shardManager.getTextChannelById(request.channel)
+
+        if (channel == null) {
+            log.error("Received SendTypingRequest for channel ${request.channel} which was not found")
+            return
+        }
+
+        channel.sendTyping().queue()
+    }
+
+}
