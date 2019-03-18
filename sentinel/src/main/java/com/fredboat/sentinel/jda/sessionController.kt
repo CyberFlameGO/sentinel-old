7b61800ff87f2c3c4d3356ccff691cdcd02e884e
Frederik Mikkelsen <fred@frederikam.com>
2019-03-18 22:06:25 +0100

    Remove dangerous lock

--- sentinel/src/main/java/com/fredboat/sentinel/jda/sessionController.kt
+++ sentinel/src/main/java/com/fredboat/sentinel/jda/sessionController.kt
@@ -58,7 +58,6 @@ class RemoteSessionController(
         localQueue.values.forEach { it.send(false) }
     }
 
-    @Synchronized
     fun onRunRequest(id: Int): String {
         val status = shardManager.getShardById(id)?.status
         log.info("Received request to run shard $id, which has status $status")
