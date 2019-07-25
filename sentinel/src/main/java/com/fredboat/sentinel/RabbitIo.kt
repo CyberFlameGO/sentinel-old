9522a2e5d5de123e0e1dd304c0803bc31380fcd0
Frederik Ar. Mikkelsen <fred@frederikam.com>
2019-07-25 13:09:35 +0200

    Cleanup exchange declaration

--- sentinel/src/main/java/com/fredboat/sentinel/RabbitIo.kt
+++ sentinel/src/main/java/com/fredboat/sentinel/RabbitIo.kt
@@ -2,14 +2,12 @@ package com.fredboat.sentinel
 
 import com.fredboat.sentinel.config.RoutingKey
 import com.rabbitmq.client.AMQP
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.reactive.awaitLast
-import kotlinx.coroutines.reactor.mono
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.rabbitmq.BindingSpecification
 import reactor.rabbitmq.ExchangeSpecification
 import reactor.rabbitmq.Receiver
 import reactor.rabbitmq.Sender
@@ -23,33 +21,41 @@ class RabbitIo(private val sender: Sender, private val receiver: Receiver, routi
 
     init {
 
+        Flux.concat(configureExchanges())
+                .count()
+                .doOnSuccess { log.info("Declared $it exchanges") }
+                .thenMany(Flux.concat(configureQueues()))
+                .count()
+                .doOnSuccess { log.info("Declared $it queues") }
+                .subscribe { log.info("Configured RabbitMQ resources") }
     }
 
-    suspend fun configureAll() {
-        Flux.concat(configureExchanges()).awaitLast()
+    private final fun configureExchanges(): List<Mono<AMQP.Exchange.DeclareOk>> {
+        return mutableListOf<Mono<AMQP.Exchange.DeclareOk>>().apply {
+            delcareExchange(SentinelExchanges.SESSIONS)
+            delcareExchange(SentinelExchanges.REQUESTS)
+            delcareExchange(SentinelExchanges.FANOUT)
+        }
     }
 
-    fun configureExchanges(): MutableList<Mono<AMQP.Exchange.DeclareOk>> {
-        val monos = mutableListOf<Mono<AMQP.Exchange.DeclareOk>>()
-        monos.add(sender.declareExchange(ExchangeSpecification().apply {
-            name(SentinelExchanges.FANOUT)
-            durable(false)
-            autoDelete(true)
-        }))
-        monos.add(sender.declareExchange(ExchangeSpecification().apply {
-            name(SentinelExchanges.REQUESTS)
-            durable(false)
-            autoDelete(true)
-        }))
-        monos.add(sender.declareExchange(ExchangeSpecification().apply {
-            name(SentinelExchanges.SESSIONS)
-            durable(false)
-            autoDelete(true)
+    private final fun configureQueues(): List<Mono<AMQP.Queue.DeclareOk>> {
+        val monos = mutableListOf<Mono<AMQP.Queue.DeclareOk>>()
+        return monos
+    }
+
+    private final fun declareBindings(): List<Mono<AMQP.Queue.BindOk>> {
+        val monos = mutableListOf<Mono<AMQP.Queue.BindOk>>()
+        monos.add(sender.bind(BindingSpecification().apply {
+
         }))
         return monos
     }
 
-    fun configureQueues() {
-
+    private final fun delcareExchange(name: String): Mono<AMQP.Exchange.DeclareOk> {
+        return sender.declareExchange(ExchangeSpecification().apply {
+            name(name)
+            durable(false)
+            autoDelete(true)
+        })
     }
 }
