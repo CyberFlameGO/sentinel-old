886c4482f200464d0cd971164072ce1e14bbf809
Frederik Ar. Mikkelsen <fred@frederikam.com>
2019-07-25 13:26:48 +0200

    Finish declaring resources

--- sentinel/src/main/java/com/fredboat/sentinel/RabbitIo.kt
+++ sentinel/src/main/java/com/fredboat/sentinel/RabbitIo.kt
@@ -1,62 +1,78 @@
 package com.fredboat.sentinel
 
+import com.fredboat.sentinel.SentinelExchanges.FANOUT
+import com.fredboat.sentinel.SentinelExchanges.REQUESTS
+import com.fredboat.sentinel.SentinelExchanges.SESSIONS
 import com.fredboat.sentinel.config.RoutingKey
-import com.rabbitmq.client.AMQP
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
-import reactor.rabbitmq.BindingSpecification
-import reactor.rabbitmq.ExchangeSpecification
-import reactor.rabbitmq.Receiver
-import reactor.rabbitmq.Sender
+import reactor.rabbitmq.*
 
 @Controller
-class RabbitIo(private val sender: Sender, private val receiver: Receiver, routingKey: RoutingKey) {
+class RabbitIo(private val sender: Sender, private val receiver: Receiver, private val routingKey: RoutingKey) {
 
     companion object {
         private val log: Logger = LoggerFactory.getLogger(RabbitIo::class.java)
     }
 
     init {
-
-        Flux.concat(configureExchanges())
+        Flux.concat(declareExchanges())
                 .count()
                 .doOnSuccess { log.info("Declared $it exchanges") }
-                .thenMany(Flux.concat(configureQueues()))
+                .thenMany(Flux.concat(declareQueues()))
                 .count()
                 .doOnSuccess { log.info("Declared $it queues") }
-                .subscribe { log.info("Configured RabbitMQ resources") }
+                .thenMany(Flux.concat(declareBindings()))
+                .count()
+                .doOnSuccess { log.info("Declared $it bindings") }
+                .subscribe { log.info("Declared all RabbitMQ resources") }
     }
 
-    private fun configureExchanges(): List<Mono<AMQP.Exchange.DeclareOk>> {
-        return mutableListOf<Mono<AMQP.Exchange.DeclareOk>>().apply {
-            delcareExchange(SentinelExchanges.SESSIONS)
-            delcareExchange(SentinelExchanges.REQUESTS)
-            delcareExchange(SentinelExchanges.FANOUT, type = "fanout")
-        }
-    }
+    private fun declareExchanges() = mutableListOf(
+            declareExchange(REQUESTS),
+            declareExchange(SESSIONS),
+            declareExchange(FANOUT, type = "fanout")
+    )
 
-    private fun configureQueues(): List<Mono<AMQP.Queue.DeclareOk>> {
-        val monos = mutableListOf<Mono<AMQP.Queue.DeclareOk>>()
-        return monos
-    }
+    private fun declareQueues() = mutableListOf(
+            declareQueue(REQUESTS),
+            declareQueue(SESSIONS),
+            declareQueue(FANOUT)
+    )
 
-    private fun declareBindings(): List<Mono<AMQP.Queue.BindOk>> {
-        val monos = mutableListOf<Mono<AMQP.Queue.BindOk>>()
-        monos.add(sender.bind(BindingSpecification().apply {
+    private fun declareBindings() = mutableListOf(
+            declareBinding(REQUESTS, REQUESTS, routingKey),
+            declareBinding(SESSIONS, SESSIONS),
+            declareBinding(FANOUT, FANOUT)
+    )
 
-        }))
-        return monos
-    }
+    private fun declareExchange(
+            name: String,
+            type: String = "direct"
+    ) = sender.declareExchange(ExchangeSpecification().apply {
+                name(name)
+                durable(false)
+                autoDelete(true)
+                type(type)
+            })
 
-    private fun delcareExchange(name: String, type: String = "direct"): Mono<AMQP.Exchange.DeclareOk> {
-        return sender.declareExchange(ExchangeSpecification().apply {
-            name(name)
-            durable(false)
-            autoDelete(true)
-            type(type)
-        })
-    }
+
+    private fun declareQueue(name: String) = sender.declareQueue(QueueSpecification().apply {
+        name(name)
+        durable(false)
+        autoDelete(true)
+        exclusive(false)
+    })
+
+    private fun declareBinding(
+            exchange: String,
+            queue: String,
+            key: RoutingKey? = null
+    ) = sender.bind(BindingSpecification().apply {
+        exchange(exchange)
+        queue(queue)
+        routingKey(key?.key)
+    })
 }
